8f1d603f5eb4d49049aa565a5e1fc095f6f74599
cmanners <Chris.Manners@aglc.ca>
2014-03-24 11:48:31 -0600

    Pushing new changes

--- source/prep/collections/Movie.cs
+++ source/prep/collections/Movie.cs
@@ -1,13 +1,62 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Movie.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The movie.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
 using System;
 
 namespace prep.collections
 {
-  public class Movie
-  {
-    public string title { get; set; }
-    public ProductionStudio production_studio { get; set; }
-    public Genre genre { get; set; }
-    public int rating { get; set; }
-    public DateTime date_published { get; set; }
-  }
+    /// <summary>
+    /// The movie.
+    /// </summary>
+    public class Movie : IEquatable<Movie>
+    {
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        public string title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the production_studio.
+        /// </summary>
+        public ProductionStudio production_studio { get; set; }
+
+        /// <summary>
+        /// Gets or sets the genre.
+        /// </summary>
+        public Genre genre { get; set; }
+
+        /// <summary>
+        /// Gets or sets the rating.
+        /// </summary>
+        public int rating { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date_published.
+        /// </summary>
+        public DateTime date_published { get; set; }
+
+        /// <summary>
+        /// The equals.
+        /// </summary>
+        /// <param name="other">
+        /// The other.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public bool Equals(Movie other)
+        {
+            return other != null
+                   && (ReferenceEquals(this, other)
+                       || other.title.Equals(title));
+        }
+    }
 }
--- source/prep/collections/MovieLibrary.cs
+++ source/prep/collections/MovieLibrary.cs
@@ -1,85 +1,273 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MovieLibrary.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The movie library.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
 using System;
 using System.Collections.Generic;
 
 namespace prep.collections
 {
-  public class MovieLibrary
-  {
-    IList<Movie> movies;
+    /// <summary>
+    /// The movie library.
+    /// </summary>
+    public class MovieLibrary
+    {
+        /// <summary>
+        /// The movies.
+        /// </summary>
+        private readonly IList<Movie> movies;
 
-    public MovieLibrary(IList<Movie> list_of_movies)
-    {
-      this.movies = list_of_movies;
-    }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MovieLibrary"/> class.
+        /// </summary>
+        /// <param name="list_of_movies">
+        /// The list_of_movies.
+        /// </param>
+        public MovieLibrary(IList<Movie> list_of_movies)
+        {
+            movies = list_of_movies;
+        }
 
-    public IEnumerable<Movie> all_movies()
-    {
-      return this.movies;
-    }
+        /// <summary>
+        /// The all_movies.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerable"/>.
+        /// </returns>
+        public IEnumerable<Movie> all_movies()
+        {
+            foreach (Movie movie in movies)
+                yield return movie;
+        }
 
-    public void add(Movie movie)
-    {
-      throw new NotImplementedException();
-    }
-    
-    public IEnumerable<Movie> all_movies_published_by_pixar()
-    {
-      throw new NotImplementedException();
-    }
+        /// <summary>
+        /// The add.
+        /// </summary>
+        /// <param name="movie">
+        /// The movie.
+        /// </param>
+        public void add(Movie movie)
+        {
+            bool found = false;
 
-    public IEnumerable<Movie> all_movies_published_by_pixar_or_disney()
-    {
-      throw new NotImplementedException();
-    }
+            foreach (Movie existingMovie in movies)
+            {
+                if (existingMovie.Equals(movie))
+                {
+                    found = true;
+                    break;
+                }
+            }
 
-    public IEnumerable<Movie> all_movies_not_published_by_pixar()
-    {
-      throw new NotImplementedException();
-    }
+            if (!found)
+                movies.Add(movie);
+        }
 
-    public IEnumerable<Movie> all_movies_published_after(int year)
-    {
-      throw new NotImplementedException();
-    }
+        /// <summary>
+        /// The all_movies_published_by_pixar.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerable"/>.
+        /// </returns>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        public IEnumerable<Movie> all_movies_published_by_pixar()
+        {
+            foreach (var movie in movies)
+                if (movie.production_studio == ProductionStudio.Pixar)
+                    yield return movie;
+        }
 
-    public IEnumerable<Movie> all_movies_published_between_years(int startingYear, int endingYear)
-    {
-      throw new NotImplementedException();
-    }
+        /// <summary>
+        /// The all_movies_published_by_pixar_or_disney.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerable"/>.
+        /// </returns>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        public IEnumerable<Movie> all_movies_published_by_pixar_or_disney()
+        {
+            foreach (var movie in movies)
+                if (movie.production_studio == ProductionStudio.Pixar || movie.production_studio == ProductionStudio.Disney)
+                    yield return movie;
+        }
 
-    public IEnumerable<Movie> all_kid_movies()
-    {
-      throw new NotImplementedException();
-    }
+        /// <summary>
+        /// The all_movies_not_published_by_pixar.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerable"/>.
+        /// </returns>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        public IEnumerable<Movie> all_movies_not_published_by_pixar()
+        {
+            foreach (var movie in movies)
+                if (movie.production_studio != ProductionStudio.Pixar)
+                    yield return movie;
+        }
 
-    public IEnumerable<Movie> all_action_movies()
-    {
-      throw new NotImplementedException();
-    }
+        /// <summary>
+        /// The all_movies_published_after.
+        /// </summary>
+        /// <param name="year">
+        /// The year.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IEnumerable"/>.
+        /// </returns>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        public IEnumerable<Movie> all_movies_published_after(int year)
+        {
+            foreach (var movie in movies)
+                if (movie.date_published.Year > year)
+                    yield return movie;
+        }
 
-    public IEnumerable<Movie> sort_all_movies_by_title_descending()
-    {
-      throw new NotImplementedException();
-    }
+        /// <summary>
+        /// The all_movies_published_between_years.
+        /// </summary>
+        /// <param name="startingYear">
+        /// The starting year.
+        /// </param>
+        /// <param name="endingYear">
+        /// The ending year.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IEnumerable"/>.
+        /// </returns>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        public IEnumerable<Movie> all_movies_published_between_years(int startingYear, int endingYear)
+        {
+            foreach (var movie in movies)
+                if (movie.date_published.Year >= startingYear && movie.date_published.Year <= endingYear)
+                    yield return movie;
+        }
 
-    public IEnumerable<Movie> sort_all_movies_by_title_ascending()
-    {
-      throw new NotImplementedException();
-    }
+        /// <summary>
+        /// The all_kid_movies.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerable"/>.
+        /// </returns>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        public IEnumerable<Movie> all_kid_movies()
+        {
+            foreach (var movie in movies)
+                if (movie.genre == Genre.kids )
+                    yield return movie;
+        }
 
-    public IEnumerable<Movie> sort_all_movies_by_movie_studio_and_year_published()
-    {
-      throw new NotImplementedException();
-    }
+        /// <summary>
+        /// The all_action_movies.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerable"/>.
+        /// </returns>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        public IEnumerable<Movie> all_action_movies()
+        {
+            foreach (var movie in movies)
+                if (movie.genre == Genre.action)
+                    yield return movie;
+        }
 
-    public IEnumerable<Movie> sort_all_movies_by_date_published_descending()
-    {
-      throw new NotImplementedException();
-    }
+        /// <summary>
+        /// The sort_all_movies_by_title_descending.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerable"/>.
+        /// </returns>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        public IEnumerable<Movie> sort_all_movies_by_title_descending()
+        {
+            string minTitle = null;
+            bool nextFound;
 
-    public IEnumerable<Movie> sort_all_movies_by_date_published_ascending()
-    {
-      throw new NotImplementedException();
+            do
+            {
+                nextFound = false;
+                Movie min = null;
+
+                foreach (var movie in movies)
+                {
+                    if (minTitle == null || minTitle.CompareTo(movie.title) < 1)
+                    {
+                        min = movie;
+                        minTitle = movie.title;
+                        nextFound = true;
+                    }
+                }
+
+                if (nextFound)
+                    yield return min;
+                
+            } while (nextFound);
+        }
+
+        /// <summary>
+        /// The sort_all_movies_by_title_ascending.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerable"/>.
+        /// </returns>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        public IEnumerable<Movie> sort_all_movies_by_title_ascending()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// The sort_all_movies_by_movie_studio_and_year_published.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerable"/>.
+        /// </returns>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        public IEnumerable<Movie> sort_all_movies_by_movie_studio_and_year_published()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// The sort_all_movies_by_date_published_descending.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerable"/>.
+        /// </returns>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        public IEnumerable<Movie> sort_all_movies_by_date_published_descending()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// The sort_all_movies_by_date_published_ascending.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerable"/>.
+        /// </returns>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        public IEnumerable<Movie> sort_all_movies_by_date_published_ascending()
+        {
+            throw new NotImplementedException();
+        }
     }
-  }
 }
