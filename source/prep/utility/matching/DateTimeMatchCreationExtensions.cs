2af993133eef9176329639504c1ae443da929ac8
Max He <shilin.he@gmail.com>
2014-03-25 09:57:05 -0600

    Pushing new changes

---
+++ source/prep/utility/matching/DateTimeMatchCreationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace prep.utility.matching
+{
+  public static class DateTimeMatchCreationExtensions
+  {
+    public static IMatchA<ItemToMatch> greater_than<ItemToMatch>(
+      this MatchCreationExtensionPoint<ItemToMatch, DateTime> extension_point, int year)
+    {
+      return new ConditionalMatch<ItemToMatch>(item => extension_point.accesor(item).Year > year);
+    }
+  }
+}
