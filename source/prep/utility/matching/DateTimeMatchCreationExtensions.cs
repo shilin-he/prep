a6a316620502ddbac57319ccc082da92a82bc88a
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2014-03-25 10:29:58 -0600

    Updated

--- source/prep/utility/matching/DateTimeMatchCreationExtensions.cs
+++ source/prep/utility/matching/DateTimeMatchCreationExtensions.cs
@@ -5,9 +5,11 @@ namespace prep.utility.matching
   public static class DateTimeMatchCreationExtensions
   {
     public static IMatchA<ItemToMatch> greater_than<ItemToMatch>(
-      this MatchCreationExtensionPoint<ItemToMatch, DateTime> extension_point, int year)
+      this MatchCreationExtensionPoint<ItemToMatch, DateTime> extension_point, 
+      int year)
     {
-      return extension_point.create_conditional_match(item => extension_point.accesor(item).Year > year);
+      var condition = Match<DateTime>.with_attribute(x => x.Year).greater_than(year);
+      return extension_point.create_conditional_match(condition);
     }
   }
 }
