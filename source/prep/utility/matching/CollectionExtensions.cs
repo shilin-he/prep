f1beb0e9ab9ee5f28fa1dcb8c983bfb3964083bd
cmanners <Chris.Manners@aglc.ca>
2014-03-25 10:56:02 -0600

    Pushing new changes

---
+++ source/prep/utility/matching/CollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace prep.utility.matching
+{
+    public static class CollectionExtensions
+    {
+        public static CollectionWhereExtensionPoint<TCollectionElement, TAttribute> where
+            <TCollectionElement, TAttribute>(this IEnumerable<TCollectionElement> collection,
+                IGetAnAttribute<TCollectionElement, TAttribute> attributeAccessor)
+        {
+            return new CollectionWhereExtensionPoint<TCollectionElement, TAttribute>(collection, attributeAccessor);
+        }
+    }
+}
---
+++ source/prep/utility/matching/CollectionWhereExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace prep.utility.matching
+{
+    public static class CollectionWhereExtensions
+    {
+        public static IEnumerable<TElementType> greater_than<TElementType>(
+            this CollectionWhereExtensionPoint<TElementType, DateTime> extensionPoint, int year)
+        {
+            var condition = Match<TElementType>.with_attribute(extensionPoint.AttributeAccessor)
+                    .greater_than(year);
+
+            return extensionPoint.Collection.all_items_matching(condition);
+        }
+    }
+}
