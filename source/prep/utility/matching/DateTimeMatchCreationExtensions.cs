5534c89e467df5ab863502ba5a2095515bcfdbee
Max He <shilin.he@gmail.com>
2014-03-25 10:04:55 -0600

    Pushing new changes

--- source/prep/utility/matching/DateTimeMatchCreationExtensions.cs
+++ source/prep/utility/matching/DateTimeMatchCreationExtensions.cs
@@ -7,7 +7,7 @@ namespace prep.utility.matching
     public static IMatchA<ItemToMatch> greater_than<ItemToMatch>(
       this MatchCreationExtensionPoint<ItemToMatch, DateTime> extension_point, int year)
     {
-      return new ConditionalMatch<ItemToMatch>(item => extension_point.accesor(item).Year > year);
+      return extension_point.create_conditional_match(item => extension_point.accesor(item).Year > year);
     }
   }
 }
